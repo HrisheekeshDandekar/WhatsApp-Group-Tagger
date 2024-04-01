f8d6ad06b76cdce29e8cd22ae25f6200d57a4ed2
HrisheekeshDandekar <81858989+HrisheekeshDandekar@users.noreply.github.com>
2024-04-01 22:27:32 +0530

    art be arting (exit art file added)

---
+++ cli_goodbye.h
@@ -0,0 +1,23 @@
+void goodbye()
+{
+    printf( "\x1b[33;2m\n\n\n\n" // Dark yellow
+    "    ______________________________     \n"
+    "  / \\                             \\. \n"
+    " |   | This has been fun!         |.   \n"
+    "  \\_ |                            |.  \n"
+    "     |                            |.   \n"
+    "     |      Developed cause       |.   \n"
+    "     |      bored...              |.   \n"
+    "     |      I don't plan on       |.   \n"
+    "     |      extending this...     |.   \n"
+    "     |                            |.   \n"
+    "     |                            |.   \n"
+    "     |             made with love |.   \n"
+    "     |             - hrisheekesh  |.   \n"
+    "     |               dandekar     |.   \n"
+    "     |   _________________________|___ \n"
+    "     |  /                            /.\n"
+    "     \\_/____________________________/.\n"
+    "\x1b[0m\n"
+    );
+}
